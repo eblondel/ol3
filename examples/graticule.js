837b8a58c65b45f33903d4794e67191e800b4974
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2016-02-22 11:00:26 +0100

    revert changes in master...

--- examples/graticule.js
+++ examples/graticule.js
@@ -23,37 +23,11 @@ var map = new ol.Map({
 
 // Create the graticule component
 var graticule = new ol.Graticule({
-  
   // the style to use for the lines, optional.
   strokeStyle: new ol.style.Stroke({
     color: 'rgba(255,120,0,0.9)',
     width: 2,
     lineDash: [0.5, 4]
-  }),
-  
-  //show labels
-  showLabels : true,
-	
-	//label positions
-	lonLabelPosition: 0.05,
-	latLabelPosition: 0.95,
-	
-	//style for longitude label
-	lonLabelStyle: new ol.style.Text({
-			font: '10px Verdana',
-			fill: new ol.style.Fill({
-				color: 'rgba(0,0,0,1)'
-			})
-	}),
-	
-	//style for latitude label
-	latLabelStyle: new ol.style.Text({
-			font: '10px Verdana',
-			offsetX: -2,
-			textBaseline: 'bottom',
-			fill: new ol.style.Fill({
-				color: 'rgba(0,0,0,1)'
-			})
-	})
+  })
 });
 graticule.setMap(map);
