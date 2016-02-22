e6996d479ab8a40c15619bc385827922258ed39e
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2016-02-22 10:49:51 +0100

    improve example

--- examples/graticule.js
+++ examples/graticule.js
@@ -28,6 +28,27 @@ var graticule = new ol.Graticule({
     color: 'rgba(255,120,0,0.9)',
     width: 2,
     lineDash: [0.5, 4]
-  })
+  }),
+  //show labels
+  showLabels : true,
+	//label positions
+	lonLabelPosition: 5,
+	latLabelPosition: 5,
+	//style for longitude label
+	lonLabelStyle: new ol.style.Text({
+			font: '10px Verdana',
+			fill: new ol.style.Fill({
+				color: 'rgba(0,0,0,1)'
+			})
+	}),
+	//style for latitude label
+	latLabelStyle: new ol.style.Text({
+			font: '10px Verdana',
+			offsetX: -2,
+			textBaseline: 'bottom',
+			fill: new ol.style.Fill({
+				color: 'rgba(0,0,0,1)'
+			})
+	})
 });
 graticule.setMap(map);
